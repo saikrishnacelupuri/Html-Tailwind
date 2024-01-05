b0cf9ef45935c84148bff9a4a52723bd967efb05
krishnacelupuri <krishnacelupuri@gmail.com>
2024-01-05 15:30:15 +0000

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,15 @@
 module.exports = {
   content: [],
   theme: {
+    colors: {
+      blue: "#1fb6ff",
+      purple: "#7e5bef",
+      pink: "#ff49db",
+      orange: "#ff7849",
+      green: "#13ce66",
+      yellow: "#ffc82c",
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
