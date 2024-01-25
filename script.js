fb5bd94e08f6d22fe6adb20ff6251d03c7a5940c
krishnacelupuri <krishnacelupuri@gmail.com>
2024-01-25 17:41:23 +0000

    changes

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-// Step 1: Get references to the input field and the submit button
-
-const textInput = document.getElementById("inputname");
-const finalBtn = document.getElementById("submitButton");
-const outputHeading = document.getElementById("outputHeading");
-
-const enteredText = textInput.value;
-
-outputHeading.textContent = enteredText;
-
-//function to do action on click
-window.alert(textInput.value);
-console.print(enteredText);
+let elements = document.getElementsByName();
